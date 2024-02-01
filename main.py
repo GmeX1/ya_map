8005afe2aba149b849905a98a2325141fedc3840
GmeX1 <gmex51@yandex.ru>
2024-02-01 19:19:58 +0300

    Доделал ui

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import os
 import sys
 
-# import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
-
-SCREEN_SIZE = [600, 450]
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('ui.ui', self)
+        self.setFixedSize(650, 450)
         self.setWindowTitle('Большая задача по Maps API. Часть №1')
+
+    def get_map(self):  # Получение изображения
+        return ''
+
+    def show_map(self):  # Вывод
+        map_image = self.get_map()
+        pixmap = QPixmap(map_image)
+        self.image.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
